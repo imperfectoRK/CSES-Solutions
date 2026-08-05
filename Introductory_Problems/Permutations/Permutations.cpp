4ea16d10b135165104b30e951225ccaf42edfe90
Imperfect <86518966+imperfectoRK@users.noreply.github.com>
2026-08-05 11:28:19 +0530

    Add C++ solution for Permutations

---
+++ Introductory_Problems/Permutations/Permutations.cpp
@@ -0,0 +1,33 @@
+/*
+ * Problem Name: Permutations
+ * Language: C++
+ * Category: Introductory_Problems
+ * Date: 2026-08-05
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int n; cin>>n;
+    if(n==1) cout<<"1"<<'\n';
+    else if(n==2 || n==3) cout<<"NO SOLUTION"<<'\n';
+    else
+    {
+        int temp=2;
+        while(temp<n) {
+            cout<<temp<<" "; temp+=2;
+        }
+        temp=1;
+        while(temp<n)
+        {
+            cout<<temp<<" "; temp+=2;
+        }
+        cout<<'\n';
+    }
+ 
+    return 0;
+}
