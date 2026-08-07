f6a978023b313af1f81e4e4368aa314b4c4bd760
Imperfect <86518966+imperfectoRK@users.noreply.github.com>
2026-08-07 07:50:38 +0530

    Add C++ solution for Palindrome_Reorder

---
+++ Introductory_Problems/Palindrome_Reorder/Palindrome_Reorder.cpp
@@ -0,0 +1,11 @@
+/*
+ * Problem Name: Palindrome_Reorder
+ * Language: C++
+ * Category: Introductory_Problems
+ * Date: 2026-08-07
+ */
+
+input/code.cpp: In function 'int main()':
+input/code.cpp:10:32: warning: unused variable 'flag' [-Wunused-variable]
+   10 |     int count=0,index=-1; bool flag=0;
+      |                                ^~~~
