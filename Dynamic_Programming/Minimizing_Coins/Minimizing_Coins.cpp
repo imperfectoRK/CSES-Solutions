6f34d3047e2296a195d0ddb7a07ff42b1b0dbfab
Imperfect <86518966+imperfectoRK@users.noreply.github.com>
2026-08-07 10:39:22 +0530

    Add C++ solution for Minimizing_Coins

---
+++ Dynamic_Programming/Minimizing_Coins/Minimizing_Coins.cpp
@@ -0,0 +1,11 @@
+/*
+ * Problem Name: Minimizing_Coins
+ * Language: C++
+ * Category: Dynamic_Programming
+ * Date: 2026-08-07
+ */
+
+input/code.cpp: In function 'int solver(int)':
+input/code.cpp:10:18: warning: comparison of integer expressions of different signedness: 'int' and 'std::vector<int>::size_type' {aka 'long unsigned int'} [-Wsign-compare]
+   10 |     for(int i=0;i<nums.size();i++)
+      |                 ~^~~~~~~~~~~~
