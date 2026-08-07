852e2de3ed2f273ca8475cc79a1b65b6106ee48f
Imperfect <86518966+imperfectoRK@users.noreply.github.com>
2026-08-07 07:51:19 +0530

    Add C++ solution for Palindrome_Reorder

--- Introductory_Problems/Palindrome_Reorder/Palindrome_Reorder.cpp
+++ Introductory_Problems/Palindrome_Reorder/Palindrome_Reorder.cpp
@@ -5,7 +5,53 @@
  * Date: 2026-08-07
  */
 
-input/code.cpp: In function 'int main()':
-input/code.cpp:10:32: warning: unused variable 'flag' [-Wunused-variable]
-   10 |     int count=0,index=-1; bool flag=0;
-      |                                ^~~~
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    string s;
+    cin >> s;
+ 
+    vector<int> freq(26, 0);
+ 
+    for(char &c : s)
+        freq[c - 'A']++;
+ 
+    int count = 0, index = -1;
+ 
+    for(int i = 0; i < 26; i++) {
+        if(freq[i] % 2 == 1) {
+            index = i;
+            count++;
+        }
+    }
+ 
+    if(count > 1) {
+        cout << "NO SOLUTION\n";
+        return 0;
+    }
+ 
+    string pal;
+ 
+    for(int i = 0; i < 26; i++) {
+        if(freq[i] != 0 && i != index) {
+            string temp = string(freq[i] / 2, i + 'A');
+            pal += temp;
+        }
+    }
+ 
+    string rev = pal;
+    reverse(rev.begin(), rev.end());
+ 
+    if(index != -1)
+        pal += char(index + 'A') + rev;
+    else
+        pal += rev;
+ 
+    cout << pal << '\n';
+ 
+    return 0;
+}
